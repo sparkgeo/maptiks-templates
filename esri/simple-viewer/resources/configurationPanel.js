ed5c30fffe641a7edd1e858cd459853dcabea6e6
Alain St. Pierre <alain@aspcanada.com>
2016-02-16 12:26:55 -0800

    fixed issue with the config panel

--- esri/simple-viewer/resources/configurationPanel.js
+++ esri/simple-viewer/resources/configurationPanel.js
@@ -1,6 +1,6 @@
 {
     "configurationSettings": [
-    {
+        {
          "category":"<b>Configure Maptiks</b>",
          "fields":[  
            {
@@ -20,137 +20,120 @@
              "value": "If you require a tracking code, you can get one <a target=\"_blank\" href=\"https://analytics.maptiks.com/login/arcgis/?next=/\">here.</a>"
            }
          ]
-      },
-      {  
-      "category":"<b>Map</b>",
-      "fields":[  
-        {  
-          "type":"webmap",
-          "label":"Select a map"
-        }
-        ,{
-               "type": "appproxies"
-            }
-      ]
-    },
-    {
-        "category": "<b>App settings</b>",
-        "fields": [            {
-               "type": "appproxies"
-            },{
-            "type": "webmap"
+      },{
+            "category": "<b>App settings</b>",
+            "fields": [
+                {
+                    "type": "webmap"
+                },{
+                    "placeHolder": "Defaults to web map title",
+                    "label": "Title:",
+                    "fieldName": "title",
+                    "type": "string",
+                    "tooltip": "Defaults to web map title"
+                },{
+                    "type":"string",
+                    "fieldName":"about",
+                    "label":"Description",
+                    "tooltip":"Enter content for the description panel",
+                    "stringFieldOption":"richtext"
+                }
+            ]
         },{
-            "placeHolder": "Defaults to web map title",
-            "label": "Title:",
-            "fieldName": "title",
-            "type": "string",
-            "tooltip": "Defaults to web map title"
-        }, 
-        {
-               "type":"string",
-               "fieldName":"about",
-               "label":"Description",
-               "tooltip":"Enter content for the description panel",
-               "stringFieldOption":"richtext"
-            }
-    ]
-    },{
-      "category": "Theme",
-      "fields":[
-      {
-            "type": "color",
-            "fieldName": "theme",
-            "tooltip": "Color theme to use",
-            "label": "Color Theme:"
+            "category": "Theme",
+            "fields":[
+                {
+                    "type": "color",
+                    "fieldName": "theme",
+                    "tooltip": "Color theme to use",
+                    "label": "Color Theme:"
+                },{
+                    "type": "color",
+                    "fieldName": "color",
+                    "tooltip": "Text color",
+                    "label": "Text Color:"
+                },{
+                    "type": "color",
+                    "fieldName": "paneltheme",
+                    "tooltip": "Color theme for the drawer",
+                    "label": "Drawer Title Theme:"
+                }
+            ]
         },{
-            "type": "color",
-            "fieldName": "color",
-            "tooltip": "Text color",
-            "label": "Text Color:"
+            "category": "Search Settings",
+            "fields":[
+                {
+                   "type":"paragraph",
+                   "value": "Enable search to allow users to find a location or data in the map. Configure the search settings to refine the experience in your app by setting the default search resource, placeholder text, etc."
+                },{
+                    "type": "boolean",
+                    "fieldName": "search",
+                    "label": "Enable search tool"
+                },{
+                  "type": "search",
+                  "fieldName": "searchConfig",
+                  "label": "Configure search tool"
+                }
+            ]
         },{
-            "type": "color",
-            "fieldName": "paneltheme",
-            "tooltip": "Color theme for the drawer",
-            "label": "Drawer Title Theme:"
-        }
+            "category": "Tools",
+            "fields":[
+                {
+                    "type": "boolean",
+                    "fieldName": "locate",
+                    "label": "Find Location"
+                },{
+                    "type": "boolean",
+                    "fieldName": "home",
+                    "label": "Home Extent Button"
+                }
+            ]
 
-      ]
-    },{
-      "category": "Search Settings",
-      "fields":[
-            {
-               "type":"paragraph",
-               "value": "Enable search to allow users to find a location or data in the map. Configure the search settings to refine the experience in your app by setting the default search resource, placeholder text, etc."
-            },
-            {
-                "type": "boolean",
-                "fieldName": "search",
-                "label": "Enable search tool"
-            },{
-              "type": "search",
-              "fieldName": "searchConfig",
-              "label": "Configure search tool"
-            }
-      ]
-    },{
-        "category": "Tools",
-        "fields":[
-            {
-                "type": "boolean",
-                "fieldName": "locate",
-                "label": "Find Location"
-            },{
-                "type": "boolean",
-                "fieldName": "home",
-                "label": "Home Extent Button"
-            }
-        ]
-
-    },{
-         "category": "Custom URL Parameter",
-         "fields": [
-            {
-               "type": "paragraph",
-               "value": "Setup the app to support a custom url parameter. For example if your map contains a feature layer with parcel information and you'd like to be able to find parcels using a url parameter you can use this section to do so. Select a layer and search field then define the name of a custom param. Once you've defined these values you can append the custom search to your application url using the custom parameter name you define. For example, if I set the custom param value to parcels a custom url would look like this index.html?parcel=3045"
-            },{
-               "placeHolder":"i.e. parcels",
-               "label":"URL param name:",
-               "fieldName":"customUrlParam",
-               "type":"string",
-               "tooltip":"Custom URL param name"
-            },{  
-               "type":"layerAndFieldSelector",
-               "fieldName":"customUrlLayer",
-               "label":"Layer to search for custom url param value",
-               "tooltip":"Url param search layer",
-               "fields":[  
-                  {  
-                     "multipleSelection":false,
-                     "fieldName":"urlField",
-                     "label":"URL param search field",
-                     "tooltip":"URL param search field"
-                  }
-               ],
-               "layerOptions":{  
-                  "supportedTypes":[  
-                     "FeatureLayer"
-                  ],
-                  "geometryTypes":[  
-                     "esriGeometryPoint",
-                     "esriGeometryLine",
-                     "esriGeometryPolygon"
-                  ]
-               }
-            }
-
-         ]
-      }],
+        },{
+            "category": "Custom URL Parameter",
+            "fields": [
+                {
+                    "type": "paragraph",
+                    "value": "Setup the app to support a custom url parameter. For example if your map contains a feature layer with parcel information and you'd like to be able to find parcels using a url parameter you can use this section to do so. Select a layer and search field then define the name of a custom param. Once you've defined these values you can append the custom search to your application url using the custom parameter name you define. For example, if I set the custom param value to parcels a custom url would look like this index.html?parcel=3045"
+                },{
+                    "placeHolder":"i.e. parcels",
+                    "label":"URL param name:",
+                    "fieldName":"customUrlParam",
+                    "type":"string",
+                    "tooltip":"Custom URL param name"
+                },{  
+                    "type":"layerAndFieldSelector",
+                    "fieldName":"customUrlLayer",
+                    "label":"Layer to search for custom url param value",
+                    "tooltip":"Url param search layer",
+                    "fields":[  
+                        {  
+                            "multipleSelection":false,
+                            "fieldName":"urlField",
+                            "label":"URL param search field",
+                            "tooltip":"URL param search field"
+                        }
+                    ],
+                    "layerOptions":
+                        {  
+                            "supportedTypes":[
+                                "FeatureLayer"
+                            ],
+                            "geometryTypes":[  
+                                "esriGeometryPoint",
+                                "esriGeometryLine",
+                                "esriGeometryPolygon"
+                            ]
+                        }
+                }
+            ]
+      }
+    ],
     "values": {
         "search": true,
         "locate": true,
         "color": "#2f4f4f",
         "theme": "#949494",
         "paneltheme": "#ededed"
-
     }
 }
